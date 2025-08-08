94936211b4f17e921c718d6683b28ee54cc35890
tempoxqc <peterdescamps.quebec@gmail.com>
2025-08-08 13:25:01 -0400

    push node version dependencies

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://soft-res-farador.onrender.com/api',
-    socketUrl: 'https://soft-res-farador.onrender.com/api'
+    socketUrl: 'https://soft-res-farador.onrender.com'
 };
