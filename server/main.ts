ad10a142d97ee8f2aed066e76e98850532957f2d
tempoxqc <peterdescamps.quebec@gmail.com>
2025-08-08 10:22:52 -0400

    push cors

--- server/main.ts
+++ server/main.ts
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:4200',
+        origin: ['https://farador.net', 'http://localhost:4200'],
         methods: ['GET', 'POST', 'PUT']
     }
 });
